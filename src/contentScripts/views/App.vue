1fa91f17e7ac452152edf202a39628031e3c9deb
TaricOv <tareq.ouf@gmail.com>
2023-05-22 03:01:15 +0300

    diabled the VDrawer file cuz the extension stopped working and an working on the fix

--- src/contentScripts/views/App.vue
+++ src/contentScripts/views/App.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import 'uno.css'
-const drawer = ref<boolean>(true)
-const toggleDrawer = (): void => {
-  /* eslint no-console: */
-  drawer.value = !drawer.value
-  console.log('drawer btn clicked: ', drawer.value)
-}
+// const drawer = ref<boolean>(true)
+// const toggleDrawer = (): void => {
+//   /* eslint no-console: */
+//   drawer.value = !drawer.value
+//   console.log('drawer btn clicked: ', drawer.value)
+// }
 </script>
 
 <template>
-  <VDrawer :drawer="drawer" />
+  <!-- <VDrawer v-model:drawer="drawer" /> -->
   <div
     class="fixed right-0 bottom-0 m-5 z-100 flex items-end font-sans select-none leading-1em flex w-10 h-10 rounded-full shadow cursor-pointer bg-red-500 hover:bg-red-700 transition-all duration-300"
-    @click="toggleDrawer"
   >
+    <!-- @click="toggleDrawer" -->
     <!--    <div
       class="bg-white text-gray-800 rounded-lg shadow w-max h-min"
       p="x-4 y-2"
